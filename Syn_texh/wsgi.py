d115303987cf9e19e186bbd17e5a86528473cd48
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2022-06-03 10:12:26 +0200

    django project Syn_texh

---
+++ Syn_texh/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Syn_texh project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Syn_texh.settings')
+
+application = get_wsgi_application()
